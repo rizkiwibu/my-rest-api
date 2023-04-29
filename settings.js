1f64230e4fa5b9ba5f840f6742bf508b0ce38a10
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-04-30 04:02:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'ikystore'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rizkiku683@gmail.com" //email
+global.email_password = "rizkiku683" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
